a8d13042f16dcf425cccc326d74d199364b118d8
takacscsaba <takacs.csaba@hotmail.com>
2018-12-23 19:27:41 +0100

    removing stuff, because it didn't work

--- week-01/day-04/hello-me.ts
+++ week-01/day-04/hello-me.ts
@@ -1,11 +1,3 @@
 let myName = 'Takács Csaba';
 
 console.log(`Hello ${myName}!`);
-
-function greeter(person: string) {
-    return `Hello ${person}!`
-  }
-  
-  const name = 'Takács Csaba'
-  
-  console.log(greeter(name))
