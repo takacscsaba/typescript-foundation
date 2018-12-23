6cf4ac3c0affd320221bfcaf4068cd8e2fef3503
takacscsaba <takacs.csaba@hotmail.com>
2018-12-23 22:23:17 +0100

    finished define-basic-info.ts

---
+++ week-01/day-04/define-basic-info.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+const 名前: string = 'タカーチ・チャバ';
+const 年: number = 23;
+const 身長: number = 180;
+const 既婚: boolean = false;
+
+console.log(`私の名前は${名前}です。`);
+console.log(`私は${年}年です。`);
+console.log(`私の身長は${身長}ｃｍです。`);
+if (既婚 === false) {
+    console.log(`私は結婚していません。`);
+} else {
+    console.log(`私は結婚しています。`);
+}
