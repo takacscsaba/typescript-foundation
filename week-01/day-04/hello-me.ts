faf390de7496928c39a21581fd471e72722758b2
takacscsaba <takacs.csaba@hotmail.com>
2018-12-23 19:26:11 +0100

    I don't know what changed

--- week-01/day-04/hello-me.ts
+++ week-01/day-04/hello-me.ts
@@ -1,3 +1,11 @@
 let myName = 'Takács Csaba';
 
 console.log(`Hello ${myName}!`);
+
+function greeter(person: string) {
+    return `Hello ${person}!`
+  }
+  
+  const name = 'Takács Csaba'
+  
+  console.log(greeter(name))
