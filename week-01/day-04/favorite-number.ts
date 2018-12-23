9eeb865685660efa7f61dc788d3ba8041978d898
takacscsaba <takacs.csaba@hotmail.com>
2018-12-23 21:39:51 +0100

    finished swap.ts

--- week-01/day-04/favorite-number.ts
+++ week-01/day-04/favorite-number.ts
@@ -3,6 +3,3 @@
 let favoriteNumber: number = 11;
 
 console.log(`My favorite number is: ${favoriteNumber}`)
-
-// Store your favorite number in a variable (as a number)
-// And print it like this: 'My favorite number is: 8'
