c911f583e033a9169d4dbef7bfcc7292fbe6bafe
takacscsaba <takacs.csaba@hotmail.com>
2018-12-24 23:05:30 +0100

    finished simple-replace.ts

---
+++ week-02/day-02/simple-replace.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let example: string = 'In a dishwasher far far away';
+console.log(example);
+
+// export = example;
+
+let newExample = example.replace("dishwasher", "galaxy");
+console.log(newExample);
