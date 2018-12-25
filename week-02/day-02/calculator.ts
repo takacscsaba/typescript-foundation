f3203028d973f780088f098e63e7bc4b68a38469
takacscsaba <takacs.csaba@hotmail.com>
2018-12-25 12:12:33 +0100

    finished calulator.ts

--- week-02/day-02/calculator.ts
+++ week-02/day-02/calculator.ts
@@ -2,40 +2,41 @@
 
 function calculate(args: string[]): number {
   if (inputChecker(args)) {
-    console.log('Good input!!');
-  
+    let operator: string = args[0];
     let operand1: number = parseFloat(args[1]);
     let operand2: number = parseFloat(args[2]);
 
+    let solution: number = 0;
+    if (operator == '+') {
+      solution = operand1 + operand2;
+    } else if (operator == '-') {
+      solution = operand1 - operand2;
+    }  else if (operator == '*') {
+      solution = operand1 * operand2;
+    }  else if (operator == '/') {
+      solution = operand1 / operand2;
+    }  else if (operator == '%') {
+      solution = operand1 % operand2;
+    } 
+
+    return solution;
+  } else {
+    console.error('Wrong input!!');
+    return;
   }
-  console.log('Wrong input!!');
-  return;
 }
 
 function inputChecker(args: string[]): boolean {
-  return true;
+  if (args.length == 3 && (args[0] == '+' || args[0] == '-' || args[0] == '*' || args[0] == '/' || args[0] == '%') && !isNaN(args[1] as any) && !isNaN(args[2] as any)) {
+    return true;
+  }
+  return false;
 }
-// Create a simple calculator application which does read the parameters from the standard input
-// and prints the result to the console.
-
-// It should support the following operations, create function called "calculate()" :
-// +, -, *, /, % and it should support two operands:
-
-// The format of the expressions must be: {operation} {operand} {operand}.
-// Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
-
-// You should use the command line arguments to accept user input
-
-// It should work like this:
-
-// Start the program with "node calculator.js + 5 6"
-// If number of arguments are not correct, print some nice errors
-// Else print the result
-// Say goodbye
 
 const args = process.argv.splice(2); // Just a helper for you to get started
-
 console.log('Input params are', args);
-calculate(args);
 
-// export = calculator;
+let calcSolution = calculate(args);
+if (calcSolution != undefined) {
+  console.log(calcSolution + "\nThank you, goodbye!");
+}
