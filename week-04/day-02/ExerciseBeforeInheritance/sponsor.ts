5571d20a6e764d52e10cf822589ddaa364682266
takacscsaba <takacs.csaba@hotmail.com>
2018-12-27 00:54:36 +0100

    finished sponsor.ts

---
+++ week-04/day-02/ExerciseBeforeInheritance/sponsor.ts
@@ -0,0 +1,25 @@
+import { Person } from "./person";
+
+export class Sponsor extends Person {
+  company: string;
+  hiredStudents: number;
+
+
+  constructor (name?: string, age?: number, gender?: string, company: string = 'Google') {
+    super(name, age, gender);
+    this.company = company;
+    this.hiredStudents = 0;
+  }
+
+  public introduce() {
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} who represents ${this.company} and hired ${this.hiredStudents} students so far`);
+  }
+
+  public getGoal() {
+    console.log("Hire brilliant junior software developers.");
+  }
+
+  public hire() {
+    this.hiredStudents++;
+  }
+}
