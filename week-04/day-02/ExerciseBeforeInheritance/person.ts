ba268f1a51b9a5101023fd6d70a2dd1967f92cd0
takacscsaba <takacs.csaba@hotmail.com>
2018-12-26 23:11:17 +0100

    init ExerciseBeforeInheritance task, person.ts created

---
+++ week-04/day-02/ExerciseBeforeInheritance/person.ts
@@ -0,0 +1,19 @@
+export class Person {
+  name: string;
+  age: number;
+  gender: string;
+
+  constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female') {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public introduce() {
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender}.`);
+  }
+
+  public getGoal() {
+    console.log("My goal is: Live for the moment!");
+  }
+}
