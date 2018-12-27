177c1619477320e327e4306a3c552ea87bc67e36
takacscsaba <takacs.csaba@hotmail.com>
2018-12-27 22:55:36 +0100

    AircraftCarrier task finished, carrier.ts finished

--- week-04/day-02/AircraftCarrier/carrier.ts
+++ week-04/day-02/AircraftCarrier/carrier.ts
@@ -1,3 +1,58 @@
+import { Aircraft } from "./aircraft";
+
 export class Carrier {
+  aircrafts: Aircraft[] = [];
+  ammoStore: number;
+  healthPoint: number;
   
+  constructor(ammoStore: number, healthPoint: number) {
+    this.ammoStore = ammoStore;
+    this.healthPoint = healthPoint;
+  }
+
+  public add(aircraft: Aircraft) {
+    this.aircrafts.push(aircraft);
+  }
+
+  public fill() {
+    let neededAmmo = this.ammoStatCalculator();
+    if (this.ammoStore == 0) {
+      throw new Error('Ammo storage is empty.');
+    } else if (this.ammoStore < neededAmmo) {
+      this.aircrafts.forEach(x => x.isPriority() ? this.ammoStore = x.refill(this.ammoStore) : 0);
+      if (this.ammoStore != 0 && neededAmmo != 0) {
+        this.aircrafts.forEach(x => x.isPriority() ? 0 : this.ammoStore = x.refill(this.ammoStore));
+      }
+    } else if (this.ammoStore > neededAmmo) {
+      this.aircrafts.forEach(x => this.ammoStore = x.refill(this.ammoStore));
+    }
+  }
+
+  public fight(otherCarrier: Carrier) {
+    this.aircrafts.forEach(x => otherCarrier.healthPoint -= x.fight());
+    otherCarrier.aircrafts.forEach(x => this.healthPoint -= x.fight());
+  }
+
+  public getStatus() {
+    if (this.healthPoint > 0) {
+      let totalDamage = this.totalDamageCalculator();
+      console.log(`HP: ${this.healthPoint}, Aircraft count: ${this.aircrafts.length}, Ammo Storage: ${this.ammoStore}, Total damage: ${totalDamage}`);
+      console.log(`Aircrafts:`);
+      this.aircrafts.forEach(x => console.log(x.getStatus()));
+    } else {
+      console.log(`It's dead Jim :(`);
+    }
+  }
+
+  public ammoStatCalculator(): number {
+    let neededAmmo: number = 0;
+    this.aircrafts.forEach(x => x.ammo == 0 ? neededAmmo += x.maxAmmo : 0);
+    return neededAmmo;
+  }
+
+  public totalDamageCalculator(): number {
+    let totalDamage: number = 0;
+    this.aircrafts.forEach(x =>  totalDamage += (x.baseDamage * x.ammo));
+    return totalDamage;
+  }
 }
--- week-04/day-02/AircraftCarrier/main.ts
+++ week-04/day-02/AircraftCarrier/main.ts
@@ -1,18 +1,44 @@
 import { F16 } from "./f16";
 import { F35 } from "./f35";
+import { Carrier } from "./carrier";
 
+// const a = new F16();
+// const s = new F35();
+// let ammo = 500;
 
-const a = new F16();
-const s = new F35();
-let ammo = 500;
+// console.log(a.getType());
+// console.log(s.getType());
+// console.log('');
+// console.log(a.getStatus());
+// console.log(s.getStatus());
+// console.log('');
+// console.log(a.refill(ammo));
+// console.log(s.refill(ammo));
+// console.log(a.getStatus());
+// console.log(s.getStatus());
 
-console.log(a.getType());
-console.log(s.getType());
+const c1 = new Carrier(54, 1000);
+const c2 = new Carrier(63, 2000);
+
+c1.add(new F16());
+c1.add(new F16());
+c1.add(new F35());
+c2.add(new F16());
+c2.add(new F35());
+c2.add(new F35());
+c1.fill();
+c2.fill();
+
+c1.getStatus();
 console.log('');
-console.log(a.getStatus());
-console.log(s.getStatus());
+c2.getStatus();
 console.log('');
-console.log(a.refill(ammo));
-console.log(s.refill(ammo));
-console.log(a.getStatus());
-console.log(s.getStatus());
+
+c1.fight(c2);
+c1.fill();
+c2.fill();
+
+c1.getStatus();
+console.log('');
+c2.getStatus();
+console.log('');
