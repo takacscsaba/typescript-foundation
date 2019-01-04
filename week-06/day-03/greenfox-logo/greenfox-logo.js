b48babed2f1a37c617e0e46ec5ab803382529480
takacscsaba <takacs.csaba@hotmail.com>
2019-01-04 16:34:12 +0100

    finished greenfox-logo task

---
+++ week-06/day-03/greenfox-logo/greenfox-logo.js
@@ -0,0 +1,5 @@
+let myLeftEye = document.getElementById('left-eye');
+let myRightEye = document.getElementById('right-eye');
+
+myLeftEye.setAttribute('fill', '#ff6347');
+myRightEye.setAttribute('fill', '#ff6347');
