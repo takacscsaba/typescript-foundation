71f4f8354efe954c1694eafa6dd1fd03d2433046
takacscsaba <takacs.csaba@hotmail.com>
2019-01-04 01:28:47 +0100

    finished createelement-3 task

---
+++ week-06/day-03/createelement-3/createelement-3.js
@@ -0,0 +1,29 @@
+const kids = [
+  { 'petName': 'Wattled crane', 'owner': 'Bryant' },
+  { 'petName': 'Devil, tasmanian', 'owner': 'Alejandro' },
+  { 'petName': 'Mynah, common', 'owner': 'Nelie' },
+  { 'petName': 'Dolphin, common', 'owner': 'Mariele' },
+  { 'petName': 'Gray duiker', 'owner': 'Maddalena' },
+  { 'petName': 'Crab (unidentified)', 'owner': 'Lucine' },
+  { 'petName': 'Ant (unidentified)', 'owner': 'Lorianna' },
+  { 'petName': 'Bison, american', 'owner': 'Tommie' },
+  { 'petName': 'Yellow mongoose', 'owner': 'Vivyan' },
+  { 'petName': 'Carpet snake', 'owner': 'Veda' },
+  { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
+];
+
+const myDiv = document.getElementById('pets');
+
+kids.forEach(kid => {
+  let myArticle = document.createElement('article');
+  let myH3 = document.createElement('h3');
+  let myP = document.createElement('p');
+
+  myH3.innerHTML = kid.owner;
+  myP.innerHTML = kid.petName;
+
+  myArticle.appendChild(myH3);
+  myArticle.appendChild(myP);
+
+  myDiv.appendChild(myArticle);
+});
