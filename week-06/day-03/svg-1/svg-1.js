9f9fb9a1a29e085d9a940ee26ec2a9f00993258f
takacscsaba <takacs.csaba@hotmail.com>
2019-01-04 17:23:05 +0100

    finished svg-01 task

---
+++ week-06/day-03/svg-1/svg-1.js
@@ -0,0 +1,7 @@
+let myRect = document.getElementById('very-rectangle');
+
+myRect.setAttribute('x', '50');
+myRect.setAttribute('y', '50');
+
+
+myRect.setAttribute('style', 'stroke:#ff0000; stroke-width: 10; fill: #ff6347');
