75e0fb21075478623d24d6e0e404702b220691f4
takacscsaba <takacs.csaba@hotmail.com>
2019-01-15 00:44:51 +0100

    finished multiple-timeout.js task

---
+++ week-07/day-02/Callback tasks/multiple-timeout.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Write a program that prints the following fruits:
+// apple -> immediately
+// pear -> after 1 seconds
+// melon -> after 3 seconds
+// grapes -> after 5 seconds
+
+console.log('apple');
+
+setTimeout(() => {
+  console.log('grapes');
+}, 5000);
+
+setTimeout(() => {
+  console.log('melon');
+}, 3000);
+
+setTimeout(() => {
+  console.log('pear');
+}, 1000);
